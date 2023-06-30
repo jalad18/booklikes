dc33d119e4d41d30c3518e11b943d45b0bbe34ff
Jalad <jaladshrimali07@gmail.com>
2023-06-30 19:37:31 +0530

    Initial commit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,4 +10,16 @@ class UsersController < ApplicationController
         @fav_books = @user.fav_books
         render json: {fav_books: @fav_books}
     end
+
+    def fetch_user_name
+        @user = User.find(params[:id])
+        @name = @user.name
+        render json: {message:"Name", name: @name}
+    end
+
+    def fetch_book_name
+        @book = Book.find(params[:id])
+        @name = @book.name
+        render json: {message:"Book name:", book_name: @name}
+    end
 end
